ed205945ce772c49f071e04f0218901ca0ab5892
Henrik Wallerman <wallermanhenrik@gmail.com>
2021-11-11 11:52:02 +0100

    Initial commit

---
+++ src/Quiz/Client.java
@@ -0,0 +1,7 @@
+package Quiz;
+
+public class Client {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Quiz/Server.java
@@ -0,0 +1,7 @@
+package Quiz;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
