7f88e8de62d52ddfb5a34f09fa3ba3fc09273136
Sharlin <sharlin.rahman91rgmail.com>
2021-11-17 20:21:56 +0100

    First approach to present categories.

--- src/Quiz/SelectCategory.java
+++ src/Quiz/SelectCategory.java
@@ -1,4 +1,53 @@
 package Quiz;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class SelectCategory {
+    private final ArrayList<String> categoryList = new ArrayList<>();
+
+    JPanel panel = new JPanel();
+
+    public void categoryPresenter() {
+        categoryList.add("Matematik");
+        categoryList.add("Marvel-filmer");
+        categoryList.add("Kampsport");
+
+        JButton cat1 = new JButton(categoryList.get(0));
+        cat1.setBackground(new Color(30, 100, 150));
+        cat1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cat1.setPreferredSize(new Dimension(150, 75));
+        panel.add(cat1);
+
+        JButton cat2 = new JButton(categoryList.get(1));
+        cat2.setBackground(new Color(30, 100, 150));
+        cat2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cat2.setPreferredSize(new Dimension(150, 75));
+        panel.add(cat2);
+
+        JButton cat3 = new JButton(categoryList.get(2));
+        cat3.setBackground(new Color(30, 100, 150));
+        cat3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cat3.setPreferredSize(new Dimension(150, 75));
+        panel.add(cat3);
+
+        cat1.addActionListener(e -> {
+            startNewGame(categoryList.get(0));
+        });
+        cat2.addActionListener(e -> {
+            startNewGame(categoryList.get(1));
+        });
+        cat3.addActionListener(e -> {
+            startNewGame(categoryList.get(2));
+        });
+    }
+
+    JPanel getPanel() {
+        return panel;
+    }
+
+    private void startNewGame(String category) {
+
+    }
 }
